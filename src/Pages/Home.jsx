f17740d69eeff935b43af148775b4414b172385b
mustafa <musmoh73@gmail.com>
2023-07-15 12:44:57 +0300

    get and post firebase

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,100 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs ,addDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [popupVisible, setPopupVisible] = useState(false);
+const [name, setName] = useState("");
+const [location, setLocation] = useState("");
+const [number, setNumber] = useState("");
+const handleBuyProduct = (product) => {
+  setPopupVisible(true);
+  // Additional logic related to buying the product
+};
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  
+  try {
+    const productData = {
+      name,
+      location,
+      number,
+    };
+    
+    // Add the productData to the Firestore collection
+    const docRef = await addDoc(collection(db, "requests"), productData);
+    console.log("Document written with ID: ", docRef.id);
+    
+    // Reset the input fields
+    setName("");
+    setLocation("");
+    setNumber("");
+    
+    // Close the popup
+    setPopupVisible(false);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+};
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productsRef = collection(db, "products"); // Replace "products" with your actual collection name
+        const snapshot = await getDocs(productsRef);
+        const productsData = snapshot.docs.map((doc) => doc.data());
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error fetching products: ", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
-    
-      <div>
-        <label htmlFor="code">code</label>
-        <input type="text" id="code" />
-      </div>
-
-      <div>
-        <p className="price" >2000</p>
-      </div>
+      {products.map((product, index) => (
+        <div key={index}>
+          <h3>{product.title}</h3>
+          <img src={product.imgUrl} alt={product.title} width={110} />
+          <p>Price: ${product.price}</p>
+          <button onClick={() => handleBuyProduct(product)}>Buy</button>
+        </div>
+      ))}
+  
+      {popupVisible && (
+        <div className="popup">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Name"
+              required
+            />
+            <input
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              placeholder="Location"
+              required
+            />
+            <input
+              type="tel"
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+              placeholder="Number"
+              required
+            />
+            <button type="submit">Buy Now</button>
+          </form>
+        </div>
+      )}
     </>
   );
+  
 };
 
 export default Home;
--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
-import 'firebase/compat/auth';
-
-
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-
+    apiKey: "AIzaSyD_xZdgsvQEIb8Bhlve0niJZOGbgISt-dg",
+    authDomain: "zao-store-e7fe8.firebaseapp.com",
+    projectId: "zao-store-e7fe8",
+    storageBucket: "zao-store-e7fe8.appspot.com",
+    messagingSenderId: "215115864210",
+    appId: "1:215115864210:web:f4acbbd46a293e4f8fae4b"
+  };
 
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
-// Initialize Firestore
-export const db = firebase.firestore();
+  const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
 
-// Initialize Storage
-export const storage = firebase.storage();
-export const auth = firebase.auth();
-
-// You can also export other Firebase services if needed
-
-export default firebase;
+export { db, storage, auth };
