877e8ffea488ad9c0d19ffa509d50131f4a2f59d
mustafa <musmoh73@gmail.com>
2023-07-15 11:12:09 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Layout from "./layout/Index";
+import { Route, Routes } from 'react-router-dom';
 
 
 
@@ -6,7 +7,9 @@ function App() {
   return (
     <>
     <Layout>
-      
+    <Routes>
+   <Route path="/" element={<Home />} />
+    </Routes>
     </Layout>
     </>
   );
