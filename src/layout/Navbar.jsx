842e81b8c17d2a980f39b8e342d529546af51352
mustafa <musmoh73@gmail.com>
2023-07-16 18:54:20 +0300

    add close BurgerMenu

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { links } from '../utils/data';
 import { Link } from 'react-router-dom';
 import BurgerMenu from '../assests/menu.png';
+import CloseMenu from '../assests/close.png'; // Import the close icon
 import "./Navbar.css"
 const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
@@ -22,13 +23,54 @@ const Navbar = () => {
     </nav>
     <img
       className='burger'
-      src={BurgerMenu}
+      src={isActive ? CloseMenu : BurgerMenu} // Change the image source based on isActive state
       onClick={handleClick}
       width={30}
       height={30}
-      alt='buger menu'
-    />
+      alt={isActive ? 'close menu' : 'burger menu'} // Update the alt text accordingly
+      />
   </header>  )
 }
 
 export default Navbar
+
+
+/*
+import React, { useState } from 'react';
+import { links } from '../utils/data';
+import { Link } from 'react-router-dom';
+import BurgerMenu from '../assests/menu.png';
+import CloseMenu from '../assests/close.png'; // Import the close icon
+import './Navbar.css';
+
+const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    setIsActive((current) => !current);
+  };
+
+  return (
+    <header className='navbar'>
+      <h1 className='logo'>zao store</h1>
+      <nav id='menu' className={isActive ? 'active' : ''}>
+        {links.map((link, i) => (
+          <Link key={i} to={link.link}>
+            {link.name}
+          </Link>
+        ))}
+      </nav>
+      <img
+        className='burger'
+        src={isActive ? CloseMenu : BurgerMenu} // Change the image source based on isActive state
+        onClick={handleClick}
+        width={30}
+        height={30}
+        alt={isActive ? 'close menu' : 'burger menu'} // Update the alt text accordingly
+      />
+    </header>
+  );
+};
+
+export default Navbar;
+
+*/ 
