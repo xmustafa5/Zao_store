1acf81f51965be31d5d33ed30cc97f5fad7c4b49
mustafa <musmoh73@gmail.com>
2023-07-18 16:06:56 +0300

    add popdone

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,76 +6,17 @@ import Popup from './Popup';
 const Cards = () => {
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [popupVisible, setPopupVisible] = useState(false);
-  const [price, setPrice] = useState(null);
   const [products, setProducts] = useState([]);
-  const [discountCode, setDiscountCode] = useState("");
-  const [name, setName] = useState("");
-  const [location, setLocation] = useState("");
-  const [number, setNumber] = useState("");
+  const [price, setPrice] = useState(null);
 
-  const handleDiscountCodeChange = (e) => {
-    setDiscountCode(e.target.value);
-  };
+  
   const handleBuyProduct = (product) => {
     setSelectedProduct(product);
     setPrice(product.price); // Set the price to the priceCode of the selected product
     setPopupVisible(true);
   };
-  const applyDiscountCode = () => {
-    try {
-      const product = products.find((p) => p.id === selectedProduct.id);
-      if (product && product.priceCode && discountCode === product.code) {
-        setPrice(product.priceCode);
-        console.log("Discount applied successfully!");
-      } else {
-        setPrice(selectedProduct.price);
-        console.log("Invalid product or discount code not available");
-      }
-    } catch (error) {
-      console.error("Error applying discount code: ", error);
-    }
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
 
-    try {
-      const productData = {
-        name,
-        location,
-        number,
-        price,
-      };
-
-      // Apply discount code if it matches the selected product
-      applyDiscountCode();
-
-      // Decrease the usageCount in all product documents
-      const productsQuerySnapshot = await getDocs(collection(db, "products"));
-      productsQuerySnapshot.forEach(async (productDoc) => {
-        const productRef = doc(db, "products", productDoc.id);
-        const productData = productDoc.data();
-        if (productData.code === discountCode && productData.usageCount && productData.usageCount > 0) {
-          const updatedUsageCount = productData.usageCount - 1;
-          await updateDoc(productRef, { usageCount: updatedUsageCount });
-        }
-      });
-
-      // Add the productData to the Firestore collection
-      const docRef = await addDoc(collection(db, "requests"), productData);
-      console.log("Document written with ID: ", docRef.id);
-
-      // Reset the input fields
-      setName("");
-      setLocation("");
-      setNumber("");
-      setDiscountCode("");
-
-      // Close the popup
-      setPopupVisible(false);
-    } catch (error) {
-      console.error("Error adding document: ", error);
-    }
-  };
+ 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
@@ -105,7 +46,7 @@ const Cards = () => {
       ))}
 
       {popupVisible && selectedProduct && (
-        <Popup/>
+        <Popup selectedProduct={selectedProduct} products={products} setPopupVisible={setPopupVisible}  setPrice={setPrice} price={price} products={products}/>
       )}
     </>
   )
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,6 +1,71 @@
-import React from 'react'
+import React, { useState,useEffect} from 'react'
+import { collection, getDocs, addDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
-const Popup = () => {
+const Popup = (setPrice,price,products,selectedProduct,setPopupVisible) => {
+    const [discountCode, setDiscountCode] = useState("");
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [number, setNumber] = useState("");
+
+    const handleDiscountCodeChange = (e) => {
+        setDiscountCode(e.target.value);
+      };
+      const applyDiscountCode = () => {
+        try {
+          const product = products.find((p) => p.id === selectedProduct.id);
+          if (product && product.priceCode && discountCode === product.code) {
+            setPrice(product.priceCode);
+            console.log("Discount applied successfully!");
+          } else {
+            setPrice(selectedProduct.price);
+            console.log("Invalid product or discount code not available");
+          }
+        } catch (error) {
+          console.error("Error applying discount code: ", error);
+        }
+      };
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          const productData = {
+            name,
+            location,
+            number,
+            price,
+          };
+    
+          // Apply discount code if it matches the selected product
+          applyDiscountCode();
+    
+          // Decrease the usageCount in all product documents
+          const productsQuerySnapshot = await getDocs(collection(db, "products"));
+          productsQuerySnapshot.forEach(async (productDoc) => {
+            const productRef = doc(db, "products", productDoc.id);
+            const productData = productDoc.data();
+            if (productData.code === discountCode && productData.usageCount && productData.usageCount > 0) {
+              const updatedUsageCount = productData.usageCount - 1;
+              await updateDoc(productRef, { usageCount: updatedUsageCount });
+            }
+          });
+    
+          // Add the productData to the Firestore collection
+          const docRef = await addDoc(collection(db, "requests"), productData);
+          console.log("Document written with ID: ", docRef.id);
+    
+          // Reset the input fields
+          setName("");
+          setLocation("");
+          setNumber("");
+          setDiscountCode("");
+    
+          // Close the popup
+          setPopupVisible(false);
+        } catch (error) {
+          console.error("Error adding document: ", error);
+        }
+      };
   return (
     <div className="popup">
           <form onSubmit={handleSubmit}>
