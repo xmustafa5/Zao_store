1328dd5218a56eaa6ef08ac60b1e77fa29a06b57
mustafa <musmoh73@gmail.com>
2023-07-18 15:56:26 +0300

    movint to popup

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,7 +2,50 @@ import React from 'react'
 
 const Popup = () => {
   return (
-    <div>Popup</div>
+    <div className="popup">
+          <form onSubmit={handleSubmit}>
+            <h3>{selectedProduct.title}</h3>
+            <img
+              src={selectedProduct.imgUrl}
+              alt={selectedProduct.title}
+              width={110}
+            />
+            <p>Price: ${price}</p>
+            {/* Display the price */}
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Name"
+              required
+            />
+            <input
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              placeholder="Location"
+              required
+            />
+            <input
+              type="tel"
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+              placeholder="Number"
+              required
+            />
+            <input
+              type="text"
+              value={discountCode}
+              onChange={handleDiscountCodeChange}
+              placeholder="Discount Code"
+              required
+            />
+            <button type="button" onClick={applyDiscountCode}>
+              Apply Code
+            </button>
+            <button type="submit">Buy Now</button>
+          </form>
+        </div>
   )
 }
 
