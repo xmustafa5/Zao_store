4d977800d09f889c8ed2a7c62a16f00f5627add9
mustafa <musmoh73@gmail.com>
2023-07-18 16:31:13 +0300

    add modelcss

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,12 +8,78 @@ const Cards = () => {
   const [popupVisible, setPopupVisible] = useState(false);
   const [products, setProducts] = useState([]);
   const [price, setPrice] = useState(null);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
+  const [discountCode, setDiscountCode] = useState("");
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [number, setNumber] = useState("");
 
+  const handleDiscountCodeChange = (e) => {
+      setDiscountCode(e.target.value);
+    };
+    const applyDiscountCode = () => {
+      try {
+        const product = products.find((p) => p.id === selectedProduct.id);
+        if (product && product.priceCode && discountCode === product.code) {
+          setPrice(product.priceCode);
+          console.log("Discount applied successfully!");
+        } else {
+          setPrice(selectedProduct.price);
+          console.log("Invalid product or discount code not available");
+        }
+      } catch (error) {
+        console.error("Error applying discount code: ", error);
+      }
+    };
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      try {
+        const productData = {
+          name,
+          location,
+          number,
+          price,
+        };
+  
+        // Apply discount code if it matches the selected product
+        applyDiscountCode();
+  
+        // Decrease the usageCount in all product documents
+        const productsQuerySnapshot = await getDocs(collection(db, "products"));
+        productsQuerySnapshot.forEach(async (productDoc) => {
+          const productRef = doc(db, "products", productDoc.id);
+          const productData = productDoc.data();
+          if (productData.code === discountCode && productData.usageCount && productData.usageCount > 0) {
+            const updatedUsageCount = productData.usageCount - 1;
+            await updateDoc(productRef, { usageCount: updatedUsageCount });
+          }
+        });
+  
+        // Add the productData to the Firestore collection
+        const docRef = await addDoc(collection(db, "requests"), productData);
+        console.log("Document written with ID: ", docRef.id);
+  
+        // Reset the input fields
+        setName("");
+        setLocation("");
+        setNumber("");
+        setDiscountCode("");
+  
+        // Close the popup
+        setPopupVisible(false);
+      } catch (error) {
+        console.error("Error adding document: ", error);
+      }
+    };
   
   const handleBuyProduct = (product) => {
     setSelectedProduct(product);
     setPrice(product.price); // Set the price to the priceCode of the selected product
-    setPopupVisible(true);
+    setIsPopupOpen(true);
+    setIsOverlayVisible(!isOverlayVisible);
+
   };
 
  
@@ -44,10 +110,61 @@ const Cards = () => {
           <button onClick={() => handleBuyProduct(product)}>Buy</button>
         </div>
       ))}
-
-      {popupVisible && selectedProduct && (
-        <Popup selectedProduct={selectedProduct} products={products} setPopupVisible={setPopupVisible}  setPrice={setPrice} price={price} products={products}/>
+        {isOverlayVisible && <div className="overlay"></div>}
+      {isPopupOpen && (
+        <div className="popuplog ">
+           <div className="modal">
+              <div onClick={handleBuyProduct} className="overlay"></div>
+              <div className="modal-content">
+          <form onSubmit={handleSubmit}>
+            {/* <h3>{selectedProduct.title}</h3>
+            <img
+              src={selectedProduct.imgUrl}
+              alt={selectedProduct.title}
+              width={110}
+            />
+            <p>Price: ${price}</p>
+            Display the price */}
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Name"
+              required
+            />
+            <input
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              placeholder="Location"
+              required
+            />
+            <input
+              type="tel"
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+              placeholder="Number"
+              required
+            />
+            <input
+              type="text"
+              value={discountCode}
+              onChange={handleDiscountCodeChange}
+              placeholder="Discount Code"
+              required
+            />
+            <button type="button" onClick={applyDiscountCode}>
+              Apply Code
+            </button>
+            <button type="submit">Buy Now</button>
+          </form>
+        </div>
+        </div>
+        </div>
       )}
+      {/* {popupVisible && selectedProduct && (
+        <Popup selectedProduct={selectedProduct} products={products} setPopupVisible={setPopupVisible}  setPrice={setPrice} price={price} products={products}/>
+      )} */}
     </>
   )
 }
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,8 +1,8 @@
 import React, { useState,useEffect} from 'react'
 import { collection, getDocs, addDoc, doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
-
-const Popup = (setPrice,price,products,selectedProduct,setPopupVisible) => {
+import "./Model.css"
+const Popup = (setPrice,price,products,selectedProduct,setPopupVisible,handleBuyProduct) => {
     const [discountCode, setDiscountCode] = useState("");
     const [name, setName] = useState("");
     const [location, setLocation] = useState("");
@@ -64,53 +64,10 @@ const Popup = (setPrice,price,products,selectedProduct,setPopupVisible) => {
           setPopupVisible(false);
         } catch (error) {
           console.error("Error adding document: ", error);
-        }
+        } 
       };
   return (
-    <div className="popup">
-          <form onSubmit={handleSubmit}>
-            <h3>{selectedProduct.title}</h3>
-            <img
-              src={selectedProduct.imgUrl}
-              alt={selectedProduct.title}
-              width={110}
-            />
-            <p>Price: ${price}</p>
-            {/* Display the price */}
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Name"
-              required
-            />
-            <input
-              type="text"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-              placeholder="Location"
-              required
-            />
-            <input
-              type="tel"
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-              placeholder="Number"
-              required
-            />
-            <input
-              type="text"
-              value={discountCode}
-              onChange={handleDiscountCodeChange}
-              placeholder="Discount Code"
-              required
-            />
-            <button type="button" onClick={applyDiscountCode}>
-              Apply Code
-            </button>
-            <button type="submit">Buy Now</button>
-          </form>
-        </div>
+   <></>
   )
 }
 
