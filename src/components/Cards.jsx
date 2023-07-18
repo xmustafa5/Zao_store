4d2eee87dec886a581389de474110f963f2f112d
mustafa <musmoh73@gmail.com>
2023-07-18 17:35:15 +0300

    add to shop

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -24,23 +24,7 @@ const Cards = () => {
     setIsOverlayVisible(!isOverlayVisible);
   };
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const productsRef = collection(db, "products");
-        const snapshot = await getDocs(productsRef);
-        const productsData = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setProducts(productsData);
-      } catch (error) {
-        console.error("Error fetching products: ", error);
-      }
-    };
 
-    fetchProducts();
-  }, []);
   if (isPopupOpen) {
     document.body.classList.add("active-modal");
   } else {
@@ -48,17 +32,64 @@ const Cards = () => {
   }
   return (
     <>
-      <p className="text-3xl ">
-        التعاون مع المصممين ومطوري الواجهة الخلفية لتحويل تصاميم واجهة المستخدم
-        إلى واجهات ويب عمليبية.
-      </p>{" "}
       {products.map((product, index) => (
-        <div key={index}>
-          <h3>{product.title}</h3>
-          <img src={product.imgUrl} alt={product.title} width={110} />
-          <p>Price: ${product.price}</p>
-          <button onClick={() => handleBuyProduct(product)}>Buy</button>
-        </div>
+        <ul className="content">
+        <li className="">
+          <div className="projcard boxs">
+            <div className="ssss">
+              <div className="projimg">
+                <img src={imageSource} alt="Selected Option" />
+              </div>
+              {/* <img src={imageSource} alt="Selected Option"  /> */}
+            </div>
+            <div className="projinfo">
+              <strong className="projtitle">
+                <span className="titlecard">{title}</span>
+                <div>
+                  {color1 && (
+                    <button
+                      className={`radio-button ${
+                        selectedOption === "option1" ? "active" : ""
+                      }`}
+                      onClick={() => handleOptionClick("option1")}
+                    >
+                      <p className="btntext2 "> {color1}</p>
+                    </button>
+                  )}
+                  {color2 && (
+                    <button
+                      className={`radio-button ${
+                        selectedOption === "option2" ? "active" : ""
+                      }`}
+                      onClick={() => handleOptionClick("option2")}
+                    >
+                      <p className="btntext2"> {color2}</p>
+                    </button>
+                  )}
+                </div>
+              </strong>
+              <div className="prices">
+                <p className="iopp">{price}$</p>
+              </div>
+            </div>
+            <div className="fexbtn">
+              {/* <button className="button-5" onClick={handleAddToBasket}>
+                Add to Basket
+              </button> */}
+              {/* <button className="button-29" onClick={handlePopupToggle}>
+                Add to Basket
+              </button> */}
+
+              <div className={"homebtngroup1"}>
+                <button className={"btnbtnprimary"} onClick={handleAddToBasket}>
+                  <p className={"btntext1"}>Buy</p>
+                  <span className={"square"}></span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
       ))}
       {isOverlayVisible && <div className="overlay"></div>}
       {isPopupOpen && (
--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import Cards from './Cards';
+import React, { useState, useEffect } from "react";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
 const Shop = () => {
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const productsRef = collection(db, "products");
+        const snapshot = await getDocs(productsRef);
+        const productsData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error fetching products: ", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <div>
-        <Cards />
+      
+      <Card
+                key={product.id}
+                title={product.title}
+                color1={product.color1}
+                color2={product.color2}
+                imageUrl1={product.imageUrl1}
+                imageUrl2={product.imageUrl2}
+                price={product.price}
+                basketItems={basketItems}
+                addToBasket={handleAddToBasket}
+                setPopupMessage={setPopupMessage}
+                setShowPopup={setShowPopup}
+                handlePopupToggle={handlePopupToggle}
+              />
+            ))}
     </div>
   )
 }
