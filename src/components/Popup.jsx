8807f434968fbd56deacb6aeee1f9541aa008bde
mustafa <musmoh73@gmail.com>
2023-07-20 14:28:29 +0300

    fix style popup

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,8 +1,14 @@
-import React, { useState,useEffect} from 'react'
-import { collection, getDocs, addDoc, doc, updateDoc } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../firebase";
-import "./Model.css"
-import "./Model.css"
+import "./Model.css";
+import { Link } from "react-router-dom";
 const Popup = ({
   setPrice,
   price,
@@ -10,75 +16,79 @@ const Popup = ({
   selectedProduct,
   setIsPopupOpen,
   setIsOverlayVisible,
-})=> {    const [discountCode, setDiscountCode] = useState("");
-    const [name, setName] = useState("");
-    const [location, setLocation] = useState("");
-    const [number, setNumber] = useState("");
- 
-    const handleDiscountCodeChange = (e) => {
-        setDiscountCode(e.target.value);
-      };
-      const applyDiscountCode = () => {
-        try {
-          const product = products.find((p) => p.id === selectedProduct.id);
-          if (product && product.priceCode && discountCode === product.code) {
-            setPrice(product.priceCode);
-            console.log("Discount applied successfully!");
-          } else {
-            setPrice(selectedProduct.price);
-            console.log("Invalid product or discount code not available");
-          }
-        } catch (error) {
-          console.error("Error applying discount code: ", error);
-        }
-      };
-   
-        const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      try {
-        const productData = {
-          name,
-          location,
-          number,
-          price,
-        };
-  
-        // Apply discount code if it matches the selected product
-        applyDiscountCode();
-  
-        // Decrease the usageCount in all product documents
-        const productsQuerySnapshot = await getDocs(collection(db, "products"));
-        productsQuerySnapshot.forEach(async (productDoc) => {
-          const productRef = doc(db, "products", productDoc.id);
-          const productData = productDoc.data();
-          if (productData.code === discountCode && productData.usageCount && productData.usageCount > 0) {
-            const updatedUsageCount = productData.usageCount - 1;
-            await updateDoc(productRef, { usageCount: updatedUsageCount });
-          }
-        });
-  
-        // Add the productData to the Firestore collection
-        const docRef = await addDoc(collection(db, "requests"), productData);
-        console.log("Document written with ID: ", docRef.id);
-  
-        // Reset the input fields
-        setName("");
-        setLocation("");
-        setNumber("");
-        setDiscountCode("");
-  
-        // Close the popup
-        setIsPopupOpen(false);
-        setIsOverlayVisible(false)
-      } catch (error) {
-        console.error("Error adding document: ", error);
-      }
-    };
-  return (
+}) => {
+  const [discountCode, setDiscountCode] = useState("");
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [number, setNumber] = useState("");
 
-<form  onSubmit={handleSubmit} className='form'>
-  {/* <h3>{selectedProduct.title}</h3>
+  const handleDiscountCodeChange = (e) => {
+    setDiscountCode(e.target.value);
+  };
+  const applyDiscountCode = () => {
+    try {
+      const product = products.find((p) => p.id === selectedProduct.id);
+      if (product && product.priceCode && discountCode === product.code) {
+        setPrice(product.priceCode);
+        console.log("Discount applied successfully!");
+      } else {
+        setPrice(selectedProduct.price);
+        console.log("Invalid product or discount code not available");
+      }
+    } catch (error) {
+      console.error("Error applying discount code: ", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const productData = {
+        name,
+        location,
+        number,
+        price,
+      };
+
+      // Apply discount code if it matches the selected product
+      applyDiscountCode();
+
+      // Decrease the usageCount in all product documents
+      const productsQuerySnapshot = await getDocs(collection(db, "products"));
+      productsQuerySnapshot.forEach(async (productDoc) => {
+        const productRef = doc(db, "products", productDoc.id);
+        const productData = productDoc.data();
+        if (
+          productData.code === discountCode &&
+          productData.usageCount &&
+          productData.usageCount > 0
+        ) {
+          const updatedUsageCount = productData.usageCount - 1;
+          await updateDoc(productRef, { usageCount: updatedUsageCount });
+        }
+      });
+
+      // Add the productData to the Firestore collection
+      const docRef = await addDoc(collection(db, "requests"), productData);
+      console.log("Document written with ID: ", docRef.id);
+
+      // Reset the input fields
+      setName("");
+      setLocation("");
+      setNumber("");
+      setDiscountCode("");
+
+      // Close the popup
+      setIsPopupOpen(false);
+      setIsOverlayVisible(false);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      {/* <h3>{selectedProduct.title}</h3>
   <img
     src={selectedProduct.imgUrl}
     alt={selectedProduct.title}
@@ -87,32 +97,78 @@ const Popup = ({
   <p>Price: ${price}</p>
   Display the price */}
 
-<div className="mb-5  inp  ">
-
-    <label for="default-input" class="block mb-2 containetinput  font-medium text-gray-900 dark:text-white">الاسم</label>
-    <input  value={name}
-    onChange={(e) => setName(e.target.value)} placeholder="name" type="text" id="default-input" class="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-</div>
-<div class="mb-5">
-    <label for="default-input" class="block mb-2  containetinput text-gray-900 dark:text-white  rtl:">العنوان</label>
-    <input  value={location}
-    onChange={(e) => setLocation(e.target.value)} type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-</div>
-<div class="mb-5">
-    <label for="default-input" class="block mb-2  containetinput text-gray-900 dark:text-white">رقم الهاتف</label>
-    <input     value={number}
-    onChange={(e) => setNumber(e.target.value)} type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-</div>
-<div class="mb-5">
-<label for="default-input" class="block mb-2  containetinput text-gray-900 dark:text-white"> كود الخصم</label>
-    <div class="relative discontinput">
-       
-        <input  value={discountCode}
-    onChange={handleDiscountCodeChange} type="search" id="search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="كود الخصم" />
-        <button onClick={applyDiscountCode} type="submit"  class="text-white absolute left-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">تاكيد</button>
-    </div>
-    </div>
-  {/* <input
+      <div className="mb-5  inp  ">
+        <label
+          for="default-input"
+          class="block mb-2 containetinput  font-medium text-gray-900 dark:text-white"
+        >
+          الاسم
+        </label>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="name"
+          type="text"
+          id="default-input"
+          class="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div class="mb-5">
+        <label
+          for="default-input"
+          class="block mb-2  containetinput text-gray-900 dark:text-white  rtl:"
+        >
+          العنوان
+        </label>
+        <input
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          type="text"
+          id="default-input"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div class="mb-5">
+        <label
+          for="default-input"
+          class="block mb-2  containetinput text-gray-900 dark:text-white"
+        >
+          رقم الهاتف
+        </label>
+        <input
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+          type="text"
+          id="default-input"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div class="mb-5">
+        <label
+          for="default-input"
+          class="block mb-2  containetinput text-gray-900 dark:text-white"
+        >
+          كود الخصم
+        </label>
+        <div class="relative discontinput">
+          <input
+            value={discountCode}
+            onChange={handleDiscountCodeChange}
+            type="search"
+            id="search"
+            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="كود الخصم"
+          />
+          <button
+            onClick={applyDiscountCode}
+            type="submit"
+            class="text-white absolute left-2.5 bottom-2.5 bg-blue-100 hover:bg-blue-800 focus:ring-2  focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-yellow-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            تاكيد
+          </button>
+        </div>
+      </div>
+      {/* <input
     type="text"
    
     placeholder="Name"
@@ -136,12 +192,36 @@ const Popup = ({
     placeholder="Discount Code"
     required
   /> */}
-  {/* <button type="button" >
+      {/* <button type="button" >
     Apply Code
   </button> */}
-  <button type="submit" className='textbtn1'>طلب الان</button>
-</form>
-  )
-}
+      <div className={"homebtngroup"}>
+        <Link to="/#shop">
+          <button
+            type="submit"
+            onClick={"handleButtonClick"}
+            className={"btnbtnprimary"}
+            data-aos="zoom-in"
+            data-aos-duration="1400"
+          >
+            <p className={"btntext3"}> شراء </p>
+            <span className={"square"}></span>
+          </button>
+        </Link>
+        {/* <Link href="/aboutus">
+              <button onClick={"handleButton1Click"}
+                className={"btnbtnsecondary"}
+                data-aos="zoom-in"
+                data-aos-duration="1400"
+              >
+                <p className={"btntext"}>Discover your current vision</p>
+                <span className={"square"}></span>
+              </button>
+            </Link> */}
+      </div>
+      {/* <button type="submit" className='btntext3'>طلب الان</button> */}
+    </form>
+  );
+};
 
-export default Popup
+export default Popup;
