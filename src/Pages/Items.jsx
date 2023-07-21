908d2704cefd77ca6426ab51b962413c1079fb8c
mustafa <musmoh73@gmail.com>
2023-07-22 02:18:41 +0300

    fix rtl

--- src/Pages/Items.jsx
+++ src/Pages/Items.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { db, storage } from "../firebase";
 import { Link } from "react-router-dom";
-import "./items.css"
+import "./items.css";
 const Items = () => {
   const [imageFile1, setImageFile1] = useState(null);
   const [imageFile2, setImageFile2] = useState(null);
@@ -109,133 +109,114 @@ const Items = () => {
 
   return (
     <div className="pro">
-<div className="contfrom">
-      <form onSubmit={handleSubmit} className="form">
-        <div className="fleoxx">
-          
-          <h1 className="fex titles pt-8">Add Item</h1>
-        </div>
-        <div className="  flex justify-center mt-4 mb-3 ">
-          <div>
-            <label
-              for="first_name"
-              className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-            >
-              Title
-            </label>
-            <input
-              type="text"
-              id="first_name"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="John"
-            />
+      <div className="contfrom">
+        <form onSubmit={handleSubmit} className="form">
+          <div className="fleoxx">
+            <h1 className="fex titles pt-8">Add Item</h1>
           </div>
-        </div>
+          <div className="  flex justify-center mt-4 mb-3 ">
+            <div>
+              <label
+                for="first_name"
+                className="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+              >
+              </label>
+              <input
+                type="text"
+                id="first_name"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="John"
+              />
+            </div>
+          </div>
 
-        <div className="flexxx">
-          <div className="flexinputs">
-            <div class="inputsss">
-              <div>
-                <label
-                  class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-                  for="file_input"
-                >
-                  glass 1
-                </label>
-                <input
-                  onChange={(e) => handleImageChange(e, setImageFile1)}
-                  class="block w-64 align-Items-center    mb-5 text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                  id="file_input"
-                  type="file"
-                />
+          <div className="flexxx">
+            <div className="flexinputs">
+              <div class="inputsss">
+                <div>
+                  <label
+                    class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+                    for="file_input"
+                  >
+                    رفع صوره
+                  </label>
+                  <input
+                    onChange={(e) => handleImageChange(e, setImageFile1)}
+                    class="block w-64 align-Items-center    mb-5 text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    id="file_input"
+                    type="file"
+                  />
+                </div>
               </div>
-              <div>
-                <label
-                  for="last_name"
-                  class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-                >
-                  color 1
-                </label>
-                <input
-                  type="text"
-                  id="last_name"
-                  value={color1}
-                  onChange={(e) => setColor1(e.target.value)}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="black"
-                />
+              <div class="inputsss">
+                <div>
+                  <label
+                    for="last_name"
+                    class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+                  >
+                    السعر
+                  </label>
+                  <input
+                    type="text"
+                    id="last_name"
+                    value={color1}
+                    onChange={(e) => setColor1(e.target.value)}
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="black"
+                  />
+                </div>
+                <div>
+                  <label
+                    for="last_name"
+                    class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+                  >
+                    السعر بعد الخصم
+                  </label>
+                  <input
+                    type="text"
+                    id="last_name"
+                    value={color2}
+                    onChange={(e) => setColor2(e.target.value)}
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="whithe"
+                  />
+                </div>
               </div>
             </div>
-            <div class="inputsss">
-              <div>
-                <label
-                  class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-                  for="file_input"
-                >
-                  glass 2
-                </label>
-                <input
-                  onChange={(e) => handleImageChange(e, setImageFile2)}
-                  class="block w-64 align-Items-center    mb-5 text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                  id="file_input"
-                  type="file"
-                />
-              </div>
-              <div>
-                <label
-                  for="last_name"
-                  class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
-                >
-                  color 2
-                </label>
-                <input
-                  type="text"
-                  id="last_name"
-                  value={color2}
-                  onChange={(e) => setColor2(e.target.value)}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="whithe"
-                />
-              </div>
-              <div></div>
+            <div>
+              <label
+                for="last_name"
+                class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+              >
+                price
+              </label>
+              <input
+                type="text"
+                id="last_name"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="200"
+              />
             </div>
-            <div></div>
-          </div>
-          <div >
-            <label
-              for="last_name"
-              class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
+            <button
+              type="submit"
+              class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-64 sm:w-auto px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              price
-            </label>
-            <input
-              type="text"
-              id="last_name"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="200"
-            />
+              رفع
+            </button>
           </div>
-          <button
-            type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-64 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-</div>  
+        </form>
+      </div>
       <div className="proo">
-        
         <h1 className="fex mt-10 text-lg mb-6 titles">items</h1>
         <div class="relative wid overflow-x-auto shadow-md rounded-md ">
           <table class="w-full text-sm text-left  text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-400 dark:text-gray-400">
               <tr>
-                <th scope="col" class="vvv center  prth  px-6 py-3"> 
+                <th scope="col" class="vvv center  prth  px-6 py-3">
                   المنتجات
                 </th>
                 <th scope="col" class=" vvv text-center prth px-6 py-3">
@@ -265,30 +246,36 @@ const Items = () => {
               // </div>
 
               <tbody key={product.id}>
-  <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-    <th
-      scope="row"
-      class="vvv center px-6 py-4 font-medium centertd whitespace-nowrap dark:text-white"
-    >
-      {product.title?.length > 0 ? (
-        product.title
-      ) : (
-        <span className="centertd">لا يوجد عنوان</span>
-      )}
-    </th>
-    <td class="vvv px-6 py-4 text-center centertd ">{product.price}</td>
-    <td class="vvv px-6 py-4 text-center centertd">{product.priceCode}</td>
-    <td class="vvv px-6 py-4 text-center centertd">{product.code}</td>
-    <td class="vvv px-6 py-4 text-center centertd">
-      <Link
-        onClick={() => handleRemoveProduct(product.id)}
-        class="font-medium center text-blue-300 dark:text-blue-200 hover:underline"
-      >
-        مسح
-      </Link>
-    </td>
-  </tr>
-</tbody>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    class="vvv center px-6 py-4 font-medium centertd whitespace-nowrap dark:text-white"
+                  >
+                    {product.title?.length > 0 ? (
+                      product.title
+                    ) : (
+                      <span className="centertd">لا يوجد عنوان</span>
+                    )}
+                  </th>
+                  <td class="vvv px-6 py-4 text-center centertd ">
+                    {product.price}
+                  </td>
+                  <td class="vvv px-6 py-4 text-center centertd">
+                    {product.priceCode}
+                  </td>
+                  <td class="vvv px-6 py-4 text-center centertd">
+                    {product.code}
+                  </td>
+                  <td class="vvv px-6 py-4 text-center centertd">
+                    <Link
+                      onClick={() => handleRemoveProduct(product.id)}
+                      class="font-medium center text-blue-300 dark:text-blue-200 hover:underline"
+                    >
+                      مسح
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
             ))}
           </table>
         </div>
