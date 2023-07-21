1283c0c8ac32a7586349655284780d20a438d6b2
mustafa <musmoh73@gmail.com>
2023-07-22 01:26:42 +0300

    style box item

--- src/Pages/Items.jsx
+++ src/Pages/Items.jsx
@@ -203,7 +203,7 @@ const Items = () => {
             </div>
             <div></div>
           </div>
-          <div>
+          <div >
             <label
               for="last_name"
               class="block mb-2 text-sm font-medium text-gray-200 dark:text-white"
@@ -218,7 +218,7 @@ const Items = () => {
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="200"
             />
-          </div>{" "}
+          </div>
           <button
             type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-64 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
