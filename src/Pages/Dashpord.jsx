f5226edfe6d6dd8875eb166c5b7bb7e1944e8fff
mustafa <musmoh73@gmail.com>
2023-07-22 03:34:23 +0300

    style dashpord

--- src/Pages/Dashpord.jsx
+++ src/Pages/Dashpord.jsx
@@ -58,6 +58,8 @@ const Dashboard = () => {
           name="usageCount"
           placeholder="Enter usage count"
           value={usageCount}
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500  w-12 text-center p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+
           onChange={(e) => setUsageCount(Number(e.target.value))}
         />
         <button type="button" onClick={handleIncrement}>
