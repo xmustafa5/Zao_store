59aec89ae9e6fbcd60018684245cd5202b1748fe
mustafa <musmoh73@gmail.com>
2023-07-22 17:31:26 +0300

    add shape

--- src/Pages/requests.jsx
+++ src/Pages/requests.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
 import { db } from '../firebase'
 import  './items.css';
 import  './requests.css';
 import  '../components/cards.css';
+import logo from "../assets/images/LogoZao.png"
+import backlogo from "../assets/images/backlogoshape.png"
 const Requests = () => {
     const [requests, setRequests] = useState([]);
 
@@ -24,16 +26,41 @@ const Requests = () => {
 
       fetchRequests();
     }, []);
-
+    const handleDeleteRequest = async (requestId) => {
+      try {
+        // Delete the document from the Firestore collection
+        await deleteDoc(doc(db, 'requests', requestId));
+        // Update the state to remove the deleted request from the list
+        setRequests((prevRequests) =>
+          prevRequests.filter((request) => request.id !== requestId)
+        );
+      } catch (error) {
+        console.error('Error deleting request: ', error);
+      }
+    };
   return (
-    <section className="pro">
+    <div className='cooe'>
+      <div className='ddde '>
+       <img src={logo} alt="logo" className='logoe z-10'  />
+       <img src={backlogo} alt="backlogo" className='absolute top-0 z-0 rotate-180' />
+      </div>
+    <section className="prfo">
     <div className="fex titles">
-      <h1>Requests</h1>
+      <h1 className='mb-3 mt-3'>الطلبات</h1>
     </div>
     {requests.length > 0 ? (
-      <ul className="content d gap-5">
+      <ul className="content d gap-5 " >
+
         {requests.map((request) => (
-          <li className="borditem " key={request.id}>
+          <li className="borditem relative " key={request.id}>
+        
+        <svg xmlns="http://www.w3.org/2000/svg" height="1.6em" viewBox="0 0 384 512"                 onClick={() => handleDeleteRequest(request.id)} // Add the onClick event handler to delete the request
+ className=' svg absolute top-2 left-2 transition-all ease-in-out hover:rotate-45 cursor-pointer'>
+  <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" fill="#e48b2a" />
+</svg>
+
+     
+
             <div className="pb-2">
               <div className="fex inputs">
                 <div className="flex w-full justify-center text-lg mb-2">
@@ -86,6 +113,7 @@ const Requests = () => {
       </div>
     )}
   </section>
+  </div>
   );
 };
 export default Requests;
