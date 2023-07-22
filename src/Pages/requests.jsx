0951a1acc1fb9caa6d27172a858fca4f7ae9f1dd
mustafa <musmoh73@gmail.com>
2023-07-22 15:57:47 +0300

    fix request component

--- src/Pages/requests.jsx
+++ src/Pages/requests.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
-import RequestsList from './RequestsList';
+import React, { useState, useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase'
+const Requests = () => {
+    const [requests, setRequests] = useState([]);
 
-const Requests= () => {
+    useEffect(() => {
+      const fetchRequests = async () => {
+        try {
+          const requestsRef = collection(db, 'requests');
+          const snapshot = await getDocs(requestsRef);
+          const requestsData = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setRequests(requestsData);
+        } catch (error) {
+          console.error('Error fetching requests: ', error);
+        }
+      };
+
+      fetchRequests();
+    }, []);
+
   return (
     <div>
-      <h1>Requests </h1>
-      <RequestsList />
-    </div>
+      <h1>Requests Page</h1>
+      <div>
+      <ul>
+        {requests.map((request) => (
+          <li key={request.id}>
+            <p>Name: {request.name}</p>
+            <p>Location: {request.location}</p>
+            <p>Number: {request.number}</p>
+            {/* Render the product data if available */}
+            {request.productData && (
+              <div>
+                <p>Product Title: {request.productData.title}</p>
+                <img src={request.productData.imgUrl} alt={request.productData.title} />
+                <p>Product Price: {request.productData.price}</p>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>    </div>
   );
 };
-
 export default Requests;
