81f0799f65ba2837872b54c6d6ac406d02dc6c87
mustafa <musmoh73@gmail.com>
2023-07-23 16:04:50 +0300

    add loading page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./loading.css";
+const Loading = (props) => (
+  <div className="loading">
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+);
+
+export default Loading;
